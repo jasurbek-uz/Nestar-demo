eb388381e4ba7b0ebceceade319ce710918d1f0e
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-09-18 13:31:33 +0900

    feat: Sass integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,17 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss"
 
 export default function App({ Component, pageProps }: AppProps) {
 	// @ts-ignore
 	const [theme, setTheme] = useState(createTheme(light));
 
-	// Socket.oi
+	// Socket.oi, Redux, Mui
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<Component {...pageProps} />
+			<Component {...pageProps} /> 
 		</ThemeProvider>
 	);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+
+// Biz bu yerda websaytimizni meta logosi nomi va rasmi internnetdan chiqib turadigan qilamiz shu faylda
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head>
+      <Head>
 				<meta charSet="UTF-8" />
-				<meta name="title" content="Nestar Demo" />
+				<meta name="title" content="Nestar Demo" /> 
 				<meta name="robots" content="index, follow" />
 				<link rel="icon" href="/img/logo/favicon.svg" />
 
 				<meta
 					name="keyword"
-					content={"nestar, nestar.uz, devex mern, mern nest js fullstack"}
+					content={"nestar, nestar.uz, devex mern, mern nest js fullstack"} // modify
 				/>
 				<meta
 					name="description"
@@ -20,7 +22,7 @@ export default function Document() {
 						"Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. |" +
 						"대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
 					}
-				/>
+        />
 			</Head>
 			<body>
 				<Main />
